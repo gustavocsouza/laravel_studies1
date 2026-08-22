aa864099262ce5a41834cf3ca48856c67de0bf85
Gustavo Cardoso <gustavo.c.souza1991@gmail.com>
2026-08-21 22:30:13 -0300

    route params with constraints

--- routes/web.php
+++ routes/web.php
@@ -7,3 +7,22 @@ Route::view('/','home');
 Route::get('/param/{id}/{id2?}', function($id, $id2 = 1) {
     echo $id . $id2;
 });
+
+
+// Route param with constraints
+
+Route::get('exp1/{value}', function($value) {
+    echo $value;
+})->where('value', '[0-9]+');
+
+Route::get('exp2/{value}', function($value) {
+    echo $value;
+})->where('value', '[A-Za-z[0-9]+');
+
+
+Route::get('exp3/{value}/{value2}', function($value, $value2) {
+    echo $value . $value2;
+})->where([
+    'value' => '[A-Za-z[0-9]+',
+    'value2' => '[0-9]+',
+]);
