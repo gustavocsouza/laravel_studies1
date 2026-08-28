118aa3b27a87cb33edd1d5bc2211c939b531f865
Gustavo Cardoso <gustavo.c.souza1991@gmail.com>
2026-08-27 23:36:07 -0300

    single action controllers and resources controllers

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,22 @@
 <?php
 
+use App\Http\Controllers\ClientController;
 use App\Http\Controllers\Main;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\SingleActionController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/init', [Main::class, 'init'])->name('init');
 Route::get('/view', [Main::class, 'viewPage'])->name('view');
+
+// route para single action controller
+Route::get('/single', SingleActionController::class)->name('single');
+
+// route para resource controller
+Route::resource('users', UserController::class);
+
+Route::resources([
+    'clients' => ClientController::class,
+    'products' => ProductController::class
+]);
