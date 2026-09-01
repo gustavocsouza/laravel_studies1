6cf1155a6f46535cf2e2c3f121d9f5125a0c36db
Gustavo Cardoso <gustavo.c.souza1991@gmail.com>
2026-08-31 23:48:08 -0300

    Admin Controller

--- routes/web.php
+++ routes/web.php
@@ -1,22 +1,6 @@
 <?php
 
-use App\Http\Controllers\ClientController;
-use App\Http\Controllers\Main;
-use App\Http\Controllers\ProductController;
-use App\Http\Controllers\SingleActionController;
-use App\Http\Controllers\UserController;
+use App\Http\Controllers\admin\AdminController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/init', [Main::class, 'init'])->name('init');
-Route::get('/view', [Main::class, 'viewPage'])->name('view');
-
-// route para single action controller
-Route::get('/single', SingleActionController::class)->name('single');
-
-// route para resource controller
-Route::resource('users', UserController::class);
-
-Route::resources([
-    'clients' => ClientController::class,
-    'products' => ProductController::class
-]);
+Route::get('/admin', [AdminController::class, 'index'])->name('admin');
