95d831819067ceb625154cc8e6fd734c3a5cfbb1
Gustavo Cardoso <gustavo.c.souza1991@gmail.com>
2026-09-13 15:26:12 -0300

    middlewares globais

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,17 @@
 <?php
 
 use App\Http\Controllers\MainController;
-use App\Http\Middleware\EndMiddleware;
-use App\Http\Middleware\StartMiddleware;
 use Illuminate\Support\Facades\Route;
 
-// Route::get('/', [MainController::class, 'index'])->name('index')->middleware([StartMiddleware::class]);
-// Route::get('/about', [MainController::class, 'about'])->name('about')->middleware([StartMiddleware::class, EndMiddleware::class]);
-// Route::get('/contact', [MainController::class, 'contact'])->name('contact');
-
-Route::middleware([StartMiddleware::class])->group(function() {
+Route::middleware('executa_antes')->group(function (){
     Route::get('/', [MainController::class, 'index'])->name('index');
-    Route::get('/about', [MainController::class, 'about'])->name('about')->withoutMiddleware([StartMiddleware::class]);
+    Route::get('/about', [MainController::class, 'about'])->name('about');
     Route::get('/contact', [MainController::class, 'contact'])->name('contact');
 });
+
+
+// Route::middleware([StartMiddleware::class])->group(function() {
+//     Route::get('/', [MainController::class, 'index'])->name('index');
+//     Route::get('/about', [MainController::class, 'about'])->name('about')->withoutMiddleware([StartMiddleware::class]);
+//     Route::get('/contact', [MainController::class, 'contact'])->name('contact');
+// });
